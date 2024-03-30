3c8ad137e0d23737ac7150903254817cd2f269ce
bishalkalicote369 <bishalkalicotecode@gmail.com>
2024-03-30 14:29:26 +0545

    chap 1

--- js-web/index.js
+++ js-web/index.js
@@ -1,5 +1,1 @@
-// calculator js
-const a = 10;
-const b = 20;
-const c = 30;
-console.log(a * b + c);
+console.log("Starting");
---
+++ js-web/intro-js/index.js
@@ -0,0 +1,20 @@
+const product = {
+  productName: "Parker jitter pen",
+  rating: 100,
+  dealOf: true,
+  price: 100,
+
+};
+console.log(product);
+
+const profile = {
+  userName: "Bishal kalicote",
+  idFollow: true,
+  message: false,
+  posts: 100,
+  followers: 100,
+  following: 100,
+  userBio: "Hello every one i'm Bishal kalicote"
+
+}
+console.log(profile);
